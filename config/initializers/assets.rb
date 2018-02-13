dfb0148f2b2444413e20d637c19306bbb99a5e16
liwen <liwenc12@gmail.com>
2018-02-12 21:03:42 -0800

    All style and js work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,19 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( components.css )
+Rails.application.config.assets.precompile += %w( responsee.css )
+Rails.application.config.assets.precompile += %w( icons.css )
+Rails.application.config.assets.precompile += %w( owl-carousel/owl-carousel.css )
+Rails.application.config.assets.precompile += %w( owl-carousel/owl-theme.css )
+Rails.application.config.assets.precompile += %w( template-style.css )
+Rails.application.config.assets.precompile += %w( jquery-1.8.3.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
+Rails.application.config.assets.precompile += %w( template-scripts.js )
+Rails.application.config.assets.precompile += %w( responsee.js )
+Rails.application.config.assets.precompile += %w( owl-carousel/owl-carousel.js )
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/about'
+  #get 'pages/about'
 
-  get 'pages/contact'
+  #get 'pages/contact'
 
-  get 'pages/resources'
+  #get 'pages/resources'
 
-  match 'about', to: 'pages#about',via: [:get, :post]
-  match 'contact', to: 'pages#contact',via: [:get, :post]
-  match 'resources', to: 'pages#resources',via: [:get, :post]
-  resources :categories
-  resources :posts
+  #match 'about', to: 'pages#about',via: [:get, :post]
+  #match 'contact', to: 'pages#contact',via: [:get, :post]
+  #match 'resources', to: 'pages#resources',via: [:get, :post]
+  #resources :categories
+  #resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
-  root 'welcome#index'
+  #get 'welcome/index'
+  #root 'welcome#index'
+  #get'/index'
+  root 'home#index'
 end
